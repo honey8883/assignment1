dbaebb2c08394ebb217d15bb9bf8396ca7817cbb
honey anto <honeyanto8883@gmail.com>
2022-06-24 07:17:40 -0500

    Moving wait logic to a function

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+output_xpath = '//*[@id="sciOutPut"]'
+
+input_xpath = '//*[@id="sciInPut"]'
+
 number_xpaths = {
     1:'//*[@id="sciout"]/tbody/tr[2]/td[2]/div/div[3]/span[1]',
     2:'//*[@id="sciout"]/tbody/tr[2]/td[2]/div/div[3]/span[2]',
@@ -36,11 +40,19 @@ def internal_click_mul(driver):
 def internal_click_div(driver):
     driver.find_element(By.XPATH,'//*[@id="sciout"]/tbody/tr[2]/td[2]/div/div[4]/span[4]').click()
 
+def internal_click_percent(driver):
+    driver.find_element(By.XPATH,'//*[@id="sciout"]/tbody/tr[2]/td[1]/div/div[5]/span[4]').click()
+
 def internal_click_neg(driver):
     driver.find_element(By.XPATH,'//*[@id="sciout"]/tbody/tr[2]/td[2]/div/div[5]/span[1]').click()
 
 def internal_click_digit(driver,digit):
     driver.find_element(By.XPATH,number_xpaths[digit]).click()
+
+
+def internal_wait_for_content(driver):
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "contentout")))
+        
 
 def test_multiplication():
     chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -50,10 +62,9 @@ def test_multiplication():
         chrome_driver.get('https://www.calculator.net')
         
         # wait for the website to load
-        content = WebDriverWait(chrome_driver, 30).until(EC.presence_of_element_located((By.ID, "contentout")))
+        internal_wait_for_content(chrome_driver)
         
-        calc_output = chrome_driver.find_element(By.XPATH,'//*[@id="sciOutPut"]')
-        calc_input = chrome_driver.find_element(By.XPATH,'//*[@id="sciInPut"]')
+        calc_output = chrome_driver.find_element(By.XPATH,output_xpath)
         internal_click_digit(chrome_driver,4)
         internal_click_digit(chrome_driver,2)
         internal_click_digit(chrome_driver,3)
@@ -75,10 +86,9 @@ def test_division():
         chrome_driver.get('https://www.calculator.net')
         
         # wait for the website to load
-        content = WebDriverWait(chrome_driver, 30).until(EC.presence_of_element_located((By.ID, "contentout")))
+        internal_wait_for_content(chrome_driver)
         
-        calc_output = chrome_driver.find_element(By.XPATH,'//*[@id="sciOutPut"]')
-        calc_input = chrome_driver.find_element(By.XPATH,'//*[@id="sciInPut"]')
+        calc_output = chrome_driver.find_element(By.XPATH,output_xpath)
         internal_click_digit(chrome_driver,4)
         internal_click_digit(chrome_driver,0)
         internal_click_digit(chrome_driver,0)
@@ -99,10 +109,9 @@ def test_addition():
         chrome_driver.get('https://www.calculator.net')
         
         # wait for the website to load
-        content = WebDriverWait(chrome_driver, 30).until(EC.presence_of_element_located((By.ID, "contentout")))
+        internal_wait_for_content(chrome_driver)
         
-        calc_output = chrome_driver.find_element(By.XPATH,'//*[@id="sciOutPut"]')
-        calc_input = chrome_driver.find_element(By.XPATH,'//*[@id="sciInPut"]')
+        calc_output = chrome_driver.find_element(By.XPATH,output_xpath)
         internal_click_neg(chrome_driver)
         internal_click_digit(chrome_driver,2)
         internal_click_digit(chrome_driver,3)
@@ -129,10 +138,9 @@ def test_substraction():
         chrome_driver.get('https://www.calculator.net')
         
         # wait for the website to load
-        content = WebDriverWait(chrome_driver, 30).until(EC.presence_of_element_located((By.ID, "contentout")))
+        internal_wait_for_content(chrome_driver)
         
-        calc_output = chrome_driver.find_element(By.XPATH,'//*[@id="sciOutPut"]')
-        calc_input = chrome_driver.find_element(By.XPATH,'//*[@id="sciInPut"]')
+        calc_output = chrome_driver.find_element(By.XPATH,output_xpath)
         
         internal_click_digit(chrome_driver,2)
         internal_click_digit(chrome_driver,3)
